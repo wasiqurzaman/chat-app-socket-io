cbb75a8be9e9b6a1ae97b44f016b528c56ebd112
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-10-19 06:03:34 +0530

    add html of the ui

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from "express";
+import path from "path";
 
 const app = express()
 const PORT = process.env.PORT || 3300;
 
 const server = app.listen(PORT, () => console.log(`Chat app is running on port ${PORT}`));
+
+const __dirname = import.meta.dirname;
+app.use(express.static(path.join(__dirname, "public")));
